e3df599d22cda0be67c601f2ce5807025a8f8629
glassc <glassc@gmail.com>
2012-06-09 17:27:43 -0600

    using mah.metro for stylinng

--- src/CQRS.Inspector/FakeEventsProvider.cs
+++ src/CQRS.Inspector/FakeEventsProvider.cs
@@ -10,11 +10,13 @@ namespace CQRS.Inspector
             return new List<EventViewModel>
                          {
                              CreateEvent( "CustomerCreatedEvent", 1, new [] {new EventPropertiesViewModel("Name", "Chris"), new EventPropertiesViewModel("Age", "29")  }),
-                             CreateEvent( "CustomerUpdatedEvent", 1, new EventPropertiesViewModel[0]),
+                             CreateEvent( "CustomerUpdatedEvent", 1, new [] {new EventPropertiesViewModel("Address", "123 Fake Street")}),
+                             CreateEvent( "ProjectCreatedEvent", 1, new [] {new EventPropertiesViewModel("Name", "Top Secret")}),
+                             CreateEvent( "TaskedAddedEvent", 1, new [] {new EventPropertiesViewModel("Name", "UI Screen Shot")}),
                          };
         }
 
-        private EventViewModel CreateEvent(string type, int number, EventPropertiesViewModel[] properties)
+        private static EventViewModel CreateEvent(string type, int number, IEnumerable<EventPropertiesViewModel> properties)
         {
             return new EventViewModel(Guid.NewGuid(), type, number, DateTime.Now, properties);
 
